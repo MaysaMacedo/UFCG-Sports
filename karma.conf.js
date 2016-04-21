ac6263ed7504ddb1e90e5b883d56db40a73188ce
André L Abrantes <andredossantosabrantes@gmail.com>
2016-04-21 12:48:02 -0300

    Configurando karma.conf para os testes

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config) {
     basePath: '',
 
     // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'mocha', 'chai-as-promised', 'chai', 'sinon-chai'],
 
     // list of files / patterns to load in the browser
     files: [
@@ -21,13 +21,8 @@ module.exports = function(config) {
       'client/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'client/bower_components/lodash/dist/lodash.compat.js',
       'client/app/app.js',
-      'client/app/app.coffee',
       'client/app/**/*.js',
-      'client/app/**/*.coffee',
       'client/components/**/*.js',
-      'client/components/**/*.coffee',
-      'client/app/**/*.jade',
-      'client/components/**/*.jade',
       'client/app/**/*.html',
       'client/components/**/*.html'
     ],
@@ -69,11 +64,20 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: false,
+
+    plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-mocha'),
+            require('karma-chai'),
+            require('karma-chai-as-promised'),
+            require('karma-sinon-chai')
+        ]
   });
 };
