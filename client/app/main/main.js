87d9408ec991f5c9e5dac40595805dbfd0361e72
maysa macedo <maysa.macedo95@gmail.com>
2016-05-08 23:30:51 -0300

    Adicionando index
    Summary:
    Merge branch 'master' of https://github.com/andreldsa/UFCG-Sports
    
    control changes in index
    
    Pagina inicial
    
    Test Plan: a ser feito
    
    Reviewers: remy.fru, andre.abrantes, Analuizamtg
    
    Differential Revision: http://andreabrantes.ddns.net/D16

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -7,5 +7,9 @@ angular.module('finalnodeApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl',
         authenticate: true
-      });
+      }).when('/main/show/:idRecurso', {
+                templateUrl: 'app/horario/calendario.show.html',
+                controller: 'CalendarioCtrl',
+                authenticate: true
+       });
   });
