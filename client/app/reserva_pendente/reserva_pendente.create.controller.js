e20b367f15e2979716afcf3a750f75066204ddfc
André L Abrantes <andredossantosabrantes@gmail.com>
2016-05-20 01:33:13 -0300

    fix chart.js
    Summary: Conenctando reserva pendente e recurso esportivo
    
    Test Plan: A fazer
    
    Reviewers: remy.fru, rafael
    
    Differential Revision: http://andreabrantes.ddns.net/D21

--- client/app/reserva_pendente/reserva_pendente.create.controller.js
+++ client/app/reserva_pendente/reserva_pendente.create.controller.js
@@ -1,25 +1,46 @@
 'use strict';
 
-angular.module('finalnodeApp')
-  .controller('ReservaPendenteCreateCtrl', function ($scope, $http, $location, Auth) {
-    $scope.reserva_pendente = {}
-    $scope.errors = {};
-    $scope.add = function(form) {
-    	$scope.submitted = true;
-      if($scope.reserva_pendente === '') {
-        return;
-      }
-      $http.post('/api/reservasPendentes', $scope.reserva_pendente).then( function() {
-    	  $location.path('/reserva_pendente')
-      }).catch(function(err) {
-    	  err = err.data;
-    	  $scope.errors = {};
+angular.module('finalnodeApp').controller('ReservaPendenteCreateCtrl', function($scope, $http, $location, Auth) {
+    var URI_RECURSO = '/api/recursos/';
 
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
-            form[field].$setValidity('mongoose', false);
-            $scope.errors[field] = error.message;
-          });
-      });
+    var PRIMEIRO_INDICE = 0;
+
+    $scope.recursosEsportivos = [];
+
+    $scope.reserva_pendente = {}
+
+    $scope.errors = {};
+
+    $scope.horariosDisponiveis = [];
+
+    $scope.selecionaHorarios = function(id) {
+      var recurso = _.where($scope.recursosEsportivos, {_id: id})[PRIMEIRO_INDICE];
+      $scope.horariosDisponiveis = recurso.horariosDisponiveis;
     };
-  });
+
+    $scope.add = function(form) {
+        $scope.submitted = true;
+        if ($scope.reserva_pendente === '') {
+            return;
+        }
+        $scope.reserva_pendente.hora = parseInt($scope.reserva_pendente.hora, 10);
+        $http.post('/api/reservasPendentes', $scope.reserva_pendente).then(function() {
+            $location.path('/reserva_pendente')
+        }).catch(function(err) {
+            err = err.data;
+            $scope.errors = {};
+
+            // Update validity of form fields that match the mongoose errors
+            angular.forEach(err.errors, function(error, field) {
+                form[field].$setValidity('mongoose', false);
+                $scope.errors[field] = error.message;
+            });
+        });
+    };
+
+    (function main() {
+        $http.get(URI_RECURSO).success(function(recursos) {
+            $scope.recursosEsportivos = recursos;
+        });
+    })();
+});
--- client/app/reserva_pendente/reserva_pendente.update.controller.js
+++ client/app/reserva_pendente/reserva_pendente.update.controller.js
@@ -1,37 +1,46 @@
 'use strict';
 
-angular.module('finalnodeApp')
-  .controller('ReservaPendenteUpdateCtrl', function ($scope, $http, $routeParams, $location, Auth) {
-	$scope.reserva_pendente = {};
-	$scope.errors = {};
-	
-	$http.get('/api/reservasPendentes/' + $routeParams.id).success(function(reserva_pendente) {
-	    $scope.reserva_pendente = reserva_pendente;
-      $scope.reserva_pendente.data = new Date($scope.reserva_pendente.data);
-	});
-	$scope.delete = function(reserva_pendente) {
-	  $http.delete('/api/reservasPendentes/' + reserva_pendente._id);
-	  $location.path('/reserva_pendente')
-	};
-	$scope.isAdmin = function() {
-		return Auth.isAdmin()
-	}
-	
+angular.module('finalnodeApp').controller('ReservaPendenteUpdateCtrl', function($scope, $http, $routeParams, $location, Auth) {
+    var URI_RECURSO = '/api/recursos/';
+
+    $scope.recursosEsportivos = [];
+
+    $scope.reserva_pendente = {};
+    $scope.errors = {};
+
+    $http.get('/api/reservasPendentes/' + $routeParams.id).success(function(reserva_pendente) {
+        $scope.reserva_pendente = reserva_pendente;
+        $scope.reserva_pendente.data = new Date($scope.reserva_pendente.data);
+    });
+    $scope.delete = function(reserva_pendente) {
+        $http.delete('/api/reservasPendentes/' + reserva_pendente._id);
+        $location.path('/reserva_pendente')
+    };
+    $scope.isAdmin = function() {
+        return Auth.isAdmin()
+    }
+
     $scope.save = function(form) {
-    	$scope.submitted = true;
-    	if(form.$valid) {
-            $http.put('/api/reservasPendentes/' + $scope.reserva_pendente._id, $scope.reserva_pendente).then( function() {
-          	  $location.path('/reserva_pendente/' + $scope.reserva_pendente._id);
+        $scope.submitted = true;
+        if (form.$valid) {
+            $http.put('/api/reservasPendentes/' + $scope.reserva_pendente._id, $scope.reserva_pendente).then(function() {
+                $location.path('/reserva_pendente/' + $scope.reserva_pendente._id);
             }).catch(function(err) {
-          	  err = err.data;
-          	  $scope.errors = {};
+                err = err.data;
+                $scope.errors = {};
 
                 // Update validity of form fields that match the mongoose errors
                 angular.forEach(err.errors, function(error, field) {
-                  form[field].$setValidity('mongoose', false);
-                  $scope.errors[field] = error.message;
+                    form[field].$setValidity('mongoose', false);
+                    $scope.errors[field] = error.message;
                 });
             });
-    	}
+        }
     };
-  });
+
+    (function main() {
+        $http.get(URI_RECURSO).success(function(recursos) {
+            $scope.recursosEsportivos = recursos;
+        });
+    })();
+});
