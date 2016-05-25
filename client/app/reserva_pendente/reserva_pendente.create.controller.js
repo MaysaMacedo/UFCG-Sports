b9a3e05f16add9a19bf2532999e4b70f79660cfc
analuizamtg <analuizamtg@gmail.com>
2016-05-25 19:46:02 -0300

    task 29

--- client/app/reserva_pendente/reserva_pendente.create.controller.js
+++ client/app/reserva_pendente/reserva_pendente.create.controller.js
@@ -4,11 +4,26 @@ angular.module('finalnodeApp')
   .controller('ReservaPendenteCreateCtrl', function ($scope, $http, $location, Auth) {
     $scope.reserva_pendente = {}
     $scope.errors = {};
+    $scope.reserva_pendente.qtdPessoas = 0;
+
+  $scope.diminuiCount = function(){
+    if ($scope.reserva_pendente.qtdPessoas > 0){
+      $scope.reserva_pendente.qtdPessoas--;
+    }
+  }
+
+    $scope.aumentaCount = function(){
+      $scope.reserva_pendente.qtdPessoas++;
+    }
+
+
     $scope.add = function(form) {
     	$scope.submitted = true;
       if($scope.reserva_pendente === '') {
         return;
-      }
+      } 
+
+
       $http.post('/api/reservasPendentes', $scope.reserva_pendente).then( function() {
     	  $location.path('/reserva_pendente')
       }).catch(function(err) {
--- client/app/reserva_pendente/reserva_pendente.update.controller.js
+++ client/app/reserva_pendente/reserva_pendente.update.controller.js
@@ -4,10 +4,13 @@ angular.module('finalnodeApp')
   .controller('ReservaPendenteUpdateCtrl', function ($scope, $http, $routeParams, $location, Auth) {
 	$scope.reserva_pendente = {};
 	$scope.errors = {};
+
 	
 	$http.get('/api/reservasPendentes/' + $routeParams.id).success(function(reserva_pendente) {
 	    $scope.reserva_pendente = reserva_pendente;
       $scope.reserva_pendente.data = new Date($scope.reserva_pendente.data);
+      $scope.countUsers = reserva_pendente.qtdPessoas;
+
 	});
 	$scope.delete = function(reserva_pendente) {
 	  $http.delete('/api/reservasPendentes/' + reserva_pendente._id);
@@ -16,10 +19,21 @@ angular.module('finalnodeApp')
 	$scope.isAdmin = function() {
 		return Auth.isAdmin()
 	}
+
+  $scope.diminuiCount = function(){
+    if ($scope.reserva_pendente.qtdPessoas > 0){
+      $scope.reserva_pendente.qtdPessoas--;
+    }
+  }
+
+  $scope.aumentaCount = function(){
+    $scope.reserva_pendente.qtdPessoas++;
+  }
 	
     $scope.save = function(form) {
     	$scope.submitted = true;
     	if(form.$valid) {
+            $scope.reserva_pendente.qtdPessoas++;
             $http.put('/api/reservasPendentes/' + $scope.reserva_pendente._id, $scope.reserva_pendente).then( function() {
           	  $location.path('/reserva_pendente/' + $scope.reserva_pendente._id);
             }).catch(function(err) {
