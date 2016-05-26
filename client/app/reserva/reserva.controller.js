cd02957aefc17f51d1f95961ee8debda37e7df0d
rafaelrds <crafards@gmail.com>
2016-05-25 21:59:04 -0300

    T25 - Telefone de contato

--- client/app/reserva/reserva.controller.js
+++ client/app/reserva/reserva.controller.js
@@ -1,15 +1,33 @@
 'use strict';
 
 angular.module('finalnodeApp')
-  .controller('ReservaIndexCtrl', function ($scope, $http, $location) {
+  .controller('ReservaIndexCtrl', function ($scope, $http, $location, Auth, User) {
     $scope.reservas = [];
+    $scope.user = {};
+    $scope.phone = ''
 
     $http.get('/api/reservas').success(function(reservas) {
       $scope.reservas = reservas;
+      // $scope.user.telefone = '9953-6749';
+      $scope.user = Auth.getCurrentUser();
+      console.log($scope.user)
+      console.log($scope.user._id);
+      console.log($scope.user.telefone);
     });
 
     $scope.noClients = function() {
       return $scope.reservas.length === 0
+    }
+
+    $scope.requirePhone = function () {
+      return typeof $scope.user.telefone == 'undefined';
+    }
+
+    $scope.addPhone = function(user) {
+      console.log('adding phone ' + $scope.phone);
+      $scope.user.telefone = $scope.phone;
+      var id = $scope.user._id;
+      $http.put('/api/users/'+id, $scope.user);
     }
     
     $scope.delete = function(reserva) {
@@ -27,4 +45,5 @@ angular.module('finalnodeApp')
     $scope.edit = function(reserva) {
           $location.path('/reserva/edit/'+reserva._id)
      };
+
   });
